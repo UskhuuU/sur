1c65091b4ecb4deff8ec65b37190c7da6a9f46d8
k <k@Usuhuu.local>
2023-11-21 21:44:45 +0800

    su8r

--- app/page.js
+++ app/page.js
@@ -1,95 +1,334 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+    <main
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexWrap: "wrap",
+        gap: "10px",
+        justifyContent: "center",
+      }}
+    >
+      <div>
+        {" "}
+        <h1>Surguuliin temtseen</h1>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexWrap: "wrap",
+          gap: "50px",
+          justifyContent: "center",
+        }}
+      >
+        <a>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/JMs_rd5_mZk?si=wVRUxRHQ18x99Nub"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </a>
+        <a>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/_mOPVBd0XrU?si=ytLmVH1s5zNWmTEF"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </a>
+        <a>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/_b93rv3HAnA?si=WoSeV30G5nSENNZ0"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </a>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexWrap: "wrap",
+          gap: "50px",
+          justifyContent: "center",
+        }}
+      >
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+          <h1>Huuhduud sonsoh du</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexWrap: "wrap",
+            gap: "50px",
+            justifyContent: "center",
+          }}
+        >
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/2cuWqHwc0v4?si=b9t5U8S96LCQwMrQ"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/i85E1aQrcu4?si=ndJqUW90l1tv7wnT"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/euNVYoMH83g?si=NyZfEbx1UzkJdLCz"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/G_dbke5pdTE?si=qtRXod13uuz6clTc"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/EovZhep-xes?si=2l1yJQeuklWL9J2b"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/jxDgapcv6cI?si=0XTCtz7r4EImRmts"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/VwiNzoqX7WU?si=BtfxUDXRnd_iaJr3"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/MSETE6ww35g?si=YTGG3DXyWBPtndKR"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/ModYrt62Vx0?si=Lzb6xhsk54H1pxke"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/T1kBJQAeQus?si=JQnzpHCptJssByHv"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/R9o1D0p_ihc?si=i5gjHImjCl_Sc0Cs"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/H8XeHQFffjU?si=x3V5Z_v0Zk0jT-n8"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/0UfQLAIf2Eg?si=pV2Qx1AvB_RJBvo-"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/WpvfVxc7ixc?si=ehBaGMUqVLa6viM3"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/RRMkO-oL9Zk?si=nb0C1PldG3h_N9TY"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/sX11TZtTuGE?si=FofhhWqyTKdP0kfz"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/JvNMW1Tqg8E?si=vveb5my9iynvWytE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexWrap: "wrap",
+          gap: "50px",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <h1>Toglolton dunduur yavah dunduu</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexWrap: "wrap",
+            gap: "50px",
+            justifyContent: "center",
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/JBYLUUPy3U4?si=Tc52hU-oBb96zsHS"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+            <p>Dovtolgoogoor onoo avah uyed ene duug 16 sekund yavuulah</p>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/Ftndim2z1l4?si=BAtOT9-AAcXKh-p0"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+            <p>Haaltaar onoo avah uyed ene duug 16 sekund yavuulah</p>
+          </a>
+          <a>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/LKGPWsuZyrI?si=9qjApc67a7wjaUTL"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+            <p>
+              Davuulaltaar onoo avah uyed ene duug 12 sekund buyuu buten
+              yavuulah
+            </p>
+          </a>
+        </div>
       </div>
     </main>
-  )
+  );
 }
